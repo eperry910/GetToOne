a59022c60653b30b8c68ba991707a767cbc5bf86
main <eperry910@yahoo.com>
2023-05-02 10:54:30 -0400

    Created coding challenge

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace GetToOne;
+public class changeToOne
+{
+    public static int reduceToOne(int startingNum)
+    {
+        while (startingNum != 1)
+        {
+            startingNum = startingNum % 2 == 0 ? startingNum / 2 : (startingNum * 3) + 1;
+            Console.WriteLine($"New number to evaluate: {startingNum}");
+        }
+        return startingNum;
+    }
+    public static void Main(string[] args)
+    {
+        reduceToOne(15);
+    }
+}
